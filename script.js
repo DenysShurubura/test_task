2d27bfaa16ca13a37e9eb4196840bfae7a159ea8
Denys <90575515+DenysShurubura@users.noreply.github.com>
2024-03-08 00:51:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.tours_slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+  });
+});
